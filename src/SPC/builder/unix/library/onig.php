6d2859bc31809f3e0b8cb01ff72c432355c75c21
cn1095 <1095053105@qq.com>
2025-03-05 19:16:15 +0800

    Update onig.php

--- src/SPC/builder/unix/library/onig.php
+++ src/SPC/builder/unix/library/onig.php
@@ -18,7 +18,7 @@ trait onig
         [,,$destdir] = SEPARATED_PATH;
 
         shell()->cd($this->source_dir)
-            ->exec('./configure --enable-static --disable-shared --prefix=')
+            ->exec('./configure --host=mipsel-linux-muslsf --enable-static --disable-shared --prefix=')
             ->exec('make clean')
             ->exec("make -j{$this->builder->concurrency}")
             ->exec("make install DESTDIR={$destdir}");
