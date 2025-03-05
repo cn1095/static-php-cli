de7e925eebde35a088a03d130c9c32fecbf09ecd
cn1095 <1095053105@qq.com>
2025-03-05 19:13:43 +0800

    Update libsodium.php

--- src/SPC/builder/unix/library/libsodium.php
+++ src/SPC/builder/unix/library/libsodium.php
@@ -9,7 +9,7 @@ trait libsodium
     protected function build(): void
     {
         shell()->cd($this->source_dir)
-            ->exec('./configure --enable-static --disable-shared --prefix=')
+            ->exec('./configure --host=mipsel-linux-muslsf --enable-static --disable-shared --prefix=')
             ->exec('make clean')
             ->exec("make -j{$this->builder->concurrency}")
             ->exec('make install DESTDIR=' . BUILD_ROOT_PATH);
